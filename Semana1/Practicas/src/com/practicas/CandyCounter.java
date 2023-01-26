0d06cdba7672c7750d7382a8d70df15240cc28e2
ivipech <ivipech@gmail.com>
2023-01-25 21:46:10 -0600

    Agruegue proyecto

--- Semana1/Practicas/src/com/practicas/CandyCounter.java
+++ Semana1/Practicas/src/com/practicas/CandyCounter.java
@@ -2,13 +2,8 @@ package com.practicas;
 
 public class CandyCounter {
 	
-	static long addCandy(double fruit, float vegetables) {
-		return (long) ((int) fruit + vegetables);
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
